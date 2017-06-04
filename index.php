0481955cfbde018341ef3a194755e92eb5963f6d
jailbreak26 <jailbreak31@gmail.com>
2017-06-04 11:50:38 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-header('Content-Type: text/html');
-?>
-<?php
 function getbetween($content, $start, $end)
 {
     $r = explode($start, $content);
@@ -16,78 +13,60 @@ function getbetween($content, $start, $end)
 }
 ?>
 <?php
-
-?>
-<iframe>
-<?php
-$url="http://cricfree.sc/update/bteu.php";
+$url="http://sports4u.ms/ch/Sky-sports-1.php";
 $agent= 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36';
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_VERBOSE, true);
-curl_setopt($ch, CURLOPT_REFERER, 'http://cricfree.sc/');
+curl_setopt($ch, CURLOPT_REFERER, 'http://sports4u.ms/');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_USERAGENT, $agent);
 curl_setopt($ch, CURLOPT_URL,$url);
 $result=curl_exec($ch);
 ?>
-
 <?php
-$start = "id='";
+$start = "channel='";
 $end = "'";
 $output = getBetween($result,$start,$end);
 ?>
-
-<iframe>
 <?php
-$url1="http://soccerschedule.online/cricembed.php?id=" . $output . "&width=620&height=490&stretching=";
-$agent= 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36';
-
+$url1="http://www.cast4u.tv/hembedplayer/" . $output . "/1/620/480";
+$agent= $_SERVER['HTTP_USER_AGENT'];
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_VERBOSE, true);
-curl_setopt($ch, CURLOPT_REFERER, $url);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_REFERER, 'http://sports4u.ms/ch/Sky-sports-1.php');
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_USERAGENT, $agent);
 curl_setopt($ch, CURLOPT_URL,$url1);
 $result1=curl_exec($ch);
 ?>
-</iframe>
 <?php
-$start = "id='";
-$end = "'";
+$start = 'enableVideo("';
+$end = '"';
 $output1 = getBetween($result1,$start,$end);
 ?>
-
-<iframe>
 <?php
-$url2="http://playstream.club/allcric.php?id=" . $output1 . "&p=0&c=0&stretching=";
-$agent= 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36';
-
+$url2="http://93.174.93.68:8088/cast4u/skys1/playlist.m3u8?id=20&pk=" . $output1 ;
+$agent= $_SERVER['HTTP_USER_AGENT'];
+$ip = $_SERVER['REMOTE_ADDR'];
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("X-Forwarded-For: $ip"));
 curl_setopt($ch, CURLOPT_VERBOSE, true);
-curl_setopt($ch, CURLOPT_REFERER, 'http://soccerschedule.online/cricembed.php?id=sky1scs&p=0&c=0&stretching=uniform');
+curl_setopt($ch, CURLOPT_REFERER, 'http://sports4u.ms/ch/Sky-sports-1.php');
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_USERAGENT, $agent);
 curl_setopt($ch, CURLOPT_URL,$url2);
-$result1=curl_exec($ch);
-?></iframe>
-<style>
-iframe {display:none !important}
-</style>
-<script>
-   var playerInstance = jwplayer("hlstester");
-   playerInstance.setup({
-   	file: "http://edge1337.herokuapp.com/<?php
-$start = 'source: "';
-$end = '"';
-$output1 = getBetween($result1,$start,$end);
-echo ($output1);
-?>",
-        <?php include 'playerconf.xhtml';?>
-</script>
+$result2=curl_exec($ch);
+$result2 = preg_replace('/http/', 'http://wizler.net/wave.php/http', $result2);
+?>
+<?php
+echo ($result2);
+?>
